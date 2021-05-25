547206d514eb933565673c16f6e351070407df77
abberispe <43879874+abberispe@users.noreply.github.com>
2021-05-25 09:55:16 +0200

    a

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="flex flex-wrap content-start ml-32">
-    <div class="w-3/6 bg-gray-200 h-half-screen"></div>
+    <div class="w-3/6 bg-gray-200 h-half-screen">
+      <Menu></Menu>
+    </div>
     <div class="w-3/6 h-half-screen" style="background-color:#e0ccb4;"></div>
-    <div class="w-3/6 h-half-screen bg-blue-600">
+    <div class="w-3/6 h-half-screen bg-gray-300">
     
     </div>
     <div class="w-3/6 bg-blue-200 h-half-screen"></div>
   </div>
-  <!--<HelloWorld msg="Hello Vue 3 + Vite" />-->
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/menu.vue'
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md

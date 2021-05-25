07ac0546b083facdf31afd9d3db017e059ea838f
abberispe <43879874+abberispe@users.noreply.github.com>
2021-05-25 08:56:18 +0200

    hallå

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-wrap content-start ml-32">
+    <div class="w-3/6 bg-gray-200 h-half-screen"></div>
+    <div class="w-3/6 h-half-screen" style="background-color:#e0ccb4;"></div>
+    <div class="w-3/6 h-half-screen bg-blue-600">
+    
+    </div>
+    <div class="w-3/6 bg-blue-200 h-half-screen"></div>
+  </div>
+  <!--<HelloWorld msg="Hello Vue 3 + Vite" />-->
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+
+.h-half-screen {
+  height:50vh;
+}
+
+</style>
